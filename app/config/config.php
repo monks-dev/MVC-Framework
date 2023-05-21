9e28bd14fa99e7c182970388622f2d152c19eefb
monks-dev <aidan.young.dev@gmail.com>
2023-05-21 05:24:49 -0400

    .htaccess ignore

---
+++ app/config/config.php
@@ -0,0 +1,18 @@
+<?php
+
+// Database Parameters
+define('DB_HOST', 'localhost');
+define('DB_USER', '_YOUR_USER_');
+define('DB_PASSWORD', '_YOUR_PASSWORD_');
+define('DB_NAME', '_YOUR_DBNAME_');
+
+// App Root
+define ('APPROOT', dirname(dirname(__FILE__)));
+// URL Root
+define ('URLROOT', '_YOUR_URL_');
+// Site Name
+define('SITENAME', '_YOUR_SITENAME_');
+
+// Header and Foot HTML
+define('HEADER', APPROOT . '/views/Inc/header.php');
+define('FOOTER', APPROOT . '/views/Inc/footer.php');
--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -3,18 +3,13 @@
 class Pages extends Controller {
     public function __construct()
     {
-        $this->postsModel = $this->model('Posts');
+
     }
 
     public function index(){
-
-        $posts = $this->postsModel->getPosts();
-
         $data =  [
             'title' => 'Home Page',
-            'posts' => $posts,
         ];
-
 
         $this->view('Pages/index', $data);
     }
